4778eabd74b57b16bf465f4e56159550224b5ec2
Jay Lee <jay0lee@gmail.com>
2020-07-30 16:55:13 -0400

    add another newline

--- scripts/analyze_image_details.py
+++ scripts/analyze_image_details.py
@@ -41,6 +41,7 @@ for data_file in data_files:
   else:
     data['analysis_version'] = analysis_version
   print(f'analyzing image for {data["image_name"]}...')
+  print()
   dl_results = common.download_image_file(data, path)
   delete_download = dl_results.get('needed_to_download', False)
   image_file_path = dl_results.get('full_file_path')
